e1e9b547ec0092bdbc647376123e9cb9f1106c4a
bringme101 <moh_abo@hotmail.com>
2016-02-27 14:20:58 -0500

    Update UI and added some of show request info

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,9 @@
 class RequestsController < ApplicationController
 
+  before_action :require_user, only: [:new, :create]
+
   def index
-    @requests = Request.select("*").joins(:user).order("id DESC")
+    @requests = Request.select("*").where('delivery_at > ?', DateTime.now).joins(:user).order("id DESC")
   end
   def new
     @request = Request.new
@@ -26,6 +28,7 @@ class RequestsController < ApplicationController
   def show
     @request = Request.find(params[:id])
     @items = @request.items
+    @location = @request.location
   end
 
   private
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to '/requests'
 
       logger.info "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
       logger.info @user.id
