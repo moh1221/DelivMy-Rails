61edf2711e797b5a140db76a0774e6e4da83a944
bringme101 <moh_abo@hotmail.com>
2016-03-04 15:35:43 -0500

    Upload image to S3 and save url in the db

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,10 +1,16 @@
 class UploadsController < ApplicationController
   def new
+    @user = current_user
+    @profile = @user.profile
   end
 
   def create
+    @user = current_user
+    @profile = @user.profile
+
     # Make an object in your bucket for your upload
-    obj = S3_BUCKET.objects[params[:file].original_filename]
+    obj = S3_BUCKET.objects[current_user.uuid[0..5] + DateTime.now.strftime("%Y%m%d%H%M%S") + ".jpg"]
+    print("File name #############" + params[:file].original_filename)
 
     # Upload the file
     obj.write(
@@ -12,22 +18,15 @@ class UploadsController < ApplicationController
         acl: :public_read
     )
 
-    # # Create an object for the upload
-    # @upload = Upload.new(
-    #     url: obj.public_url,
-    #     name: obj.key
-    # )
-    #
-    # # Save the upload
-    # if @upload.save
-    #   redirect_to uploads_path, success: 'File successfully uploaded'
-    # else
-    #   flash.now[:notice] = 'There was an error'
-    #   render :new
-    # end
+
+    # Create an object for the upload
+    @upload = @profile.update_attributes(
+        picture: obj.public_url
+    )
+    redirect_to profile_path, success: 'File successfully uploaded'
+
   end
 
   def index
-    @uploads = Upload.all
   end
 end
