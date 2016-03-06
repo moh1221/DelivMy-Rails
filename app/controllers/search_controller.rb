207b6ec2783aa46cc6636c0c36f90a43a3c2155b
bringme101 <moh_abo@hotmail.com>
2016-03-05 23:56:24 -0500

    added items count and icons to mean

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,8 @@ class SearchController < ApplicationController
     @search = Request.select("requests.id, requests.PlaceName, requests.created_at, cost, fees, delivery_at, users.first_name, users.last_name, users.email, CatName, picture")
                   .where('requests.delivery_at > ? and requests.user_id != ? and requests.StatId = 1', DateTime.now, current_user)
                   .joins(:user).joins(:profile)
-                  .joins(:category)
+                  .joins(:category).includes(:location)
+                  .includes(:items)
                   .order("requests.id DESC")
   end
 
